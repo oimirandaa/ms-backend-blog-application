31fadb5ed1c649a681c70824bd23a7e83e326890
Oscar Miranda <oimirandaa08@gmail.com>
2025-02-26 14:47:46 -0600

    Added Error Handling

---
+++ src/main/java/dev/oscarmiranda/projects/blog_backend/controllers/ErrorController.java
@@ -0,0 +1,38 @@
+package dev.oscarmiranda.projects.blog_backend.controllers;
+
+import dev.oscarmiranda.projects.blog_backend.domain.DTOs.ApiErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception exception) {
+        log.error("Caught exception ", exception);
+
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected Error Ocurred")
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException exception) {
+        //TODO Change message for prod
+        ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(exception.getMessage())
+                .build();
+
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
